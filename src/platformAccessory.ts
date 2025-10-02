9194b0b2a3613412cd5e7230bcb3164c5e5df5e2
Alberto Piva <al.piva@reply.it>
2025-10-02 23:31:33 +0200

    test

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,4 +1,4 @@
-import type { PlatformAccessory } from 'homebridge';
+import type { CharacteristicValue, PlatformAccessory } from 'homebridge';
 
 import type { ShellyPlatform } from './platform.js';
 import { DeviceConfig } from './@types/config.js';
@@ -47,5 +47,50 @@ export class ShellyPlatformAccessory {
         });
       }
     });
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   */
+  async setOn(value: CharacteristicValue) {
+    this.logger.debug('EXTERNAL: Set Characteristic On ->', value);
+    // implement your own code to turn your device on/off
+    // this.switch0_active = value as boolean;
+
+    // this.shellyDeviceState.ws?.send('Switch.Toggle', { id: 0 });
+
+    // this.platform.log.debug('Set Characteristic On ->', value);
+  }
+
+  /**
+     * Handle the "GET" requests from HomeKit
+     * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
+     *
+     * GET requests should return as fast as possible. A long delay here will result in
+     * HomeKit being unresponsive and a bad user experience in general.
+     *
+     * If your device takes time to respond you should update the status of your device
+     * asynchronously instead using the `updateCharacteristic` method instead.
+     * In this case, you may decide not to implement `onGet` handlers, which may speed up
+     * the responsiveness of your device in the Home app.
+  
+     * @example
+     * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+     */
+  async getOn(): Promise<CharacteristicValue> {
+    // implement your own code to check if the device is on
+    // const isOn = this.switch0_active;
+
+    this.platform.log.debug('EXTERNAL: Get Characteristic On ->', isOn);
+
+    // if you need to return an error to show the device as "Not Responding" in the Home app:
+    // if (isOn instanceof Error) {
+    //   throw new this.platform.api.hap.HapStatusError(
+    //     this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE,
+    //   );
+    // }
+
+    return true;
   }
 }
